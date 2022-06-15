e88b73525e1596fae1598585ed62579157522249
JennyferLima <ojennyferlima@gmail.com>
2022-06-14 21:37:42 -0300

    Primeiro envio

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let nome = prompt("Digite seu nome");
+
+let anoNascimento = prompt("Digite seu ano de nascimento");
+
+let cidade = prompt("Digite sua cidade");
+
+let anoAtual = 2022;
+
+let idade = anoAtual - anoNascimento;
+
+alert(`Eu sou o ${nome} tenho ${idade} anos e moro em ${cidade}.`);
